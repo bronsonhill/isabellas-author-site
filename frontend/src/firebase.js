b2ef127a84b33663a5fc79ccb84461c98bdffea8
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-02-06 14:18:19 +1100

    Remove debug console logs from Firebase configuration

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,12 +5,6 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Debug environment variables
-console.log('Environment Variables:', {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-});
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
