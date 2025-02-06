3391fb3429010939c01e8b1c0a0a1b647aad980f
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-02-06 18:46:23 +1100

    Refactor About section layout and styling for improved readability and responsiveness

--- frontend/src/components/SectionHomeAbout.js
+++ frontend/src/components/SectionHomeAbout.js
@@ -16,16 +16,15 @@ const HomeSection1 = () => {
         <section ref={aboutRef} className={`about-section ${isAboutVisible ? 'visible' : ''}`}>
           <h2>About Isabella</h2>
           <section className="about-content">
-              <p>
+            <div className="columns">
+              <p className="about-paragraph">
                 Isabella Eichler-Onus is a proud Gunditjmara writer living in Naarm (Melbourne). 
-                She is studying a BA majoring in Creative Writing at the University of Melbourne 
-                and has recently published poetry with various Australian literary magazines. 
+                She is studying a Bachelor of Arts and majoring in Creative Writing at the University of Melbourne.
+                She has recently published poetry with various Australian literary magazines. 
                 Her poetry and short fiction explore themes of identity, cultural connection and 
-                the intricate relationship between land and body. She draws inspiration from great 
-                Indigenous writers such as{' '}
+                the intricate relationship between land and body. 
+                She draws inspiration from great Indigenous writers such as{' '}
                 <a href="https://amzn.to/4jKORjA" target="_blank" rel="noopener noreferrer">Oodgeroo Noonuccal</a>,{' '}
-              </p>
-              <p>
                 <a href="https://amzn.to/40PVUPx" target="_blank" rel="noopener noreferrer">Tony Birch</a>,{' '}
                 <a href="https://amzn.to/3WQmDKt" target="_blank" rel="noopener noreferrer">Evelyn Araluen</a>, and{' '}
                 <a href="https://amzn.to/413z0Wg" target="_blank" rel="noopener noreferrer">Maxine Beneba Clarke</a>,
@@ -40,6 +39,7 @@ const HomeSection1 = () => {
                 on Instagram{' '}
                 <a href="https://www.instagram.com/bellieeichler" target="_blank" rel="noopener noreferrer">@bellieeichler</a>. 
               </p>
+            </div>
           </section>
         </section>
       </div>
