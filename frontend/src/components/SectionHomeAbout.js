b105272f90d40a01592a83e70be58fdade022fe1
bronsonh <bronson.hill@student.unimelb.edu.au>
2025-02-06 15:05:57 +1100

    Enhance About section with updated content and add links with hover effect

--- frontend/src/components/SectionHomeAbout.js
+++ frontend/src/components/SectionHomeAbout.js
@@ -8,7 +8,6 @@ const HomeSection1 = () => {
 
   return (
     <div className="section-background">
-        
         <div className="home-container">
         <section className="image-section">
           <img src={placeholderImage} alt="Profile Placeholder" className="profile-image" />
@@ -18,16 +17,28 @@ const HomeSection1 = () => {
           <h2>About Isabella</h2>
           <section className="about-content">
               <p>
-              Isabella Eichler-Onus is a proud Gunditjmara writer living in Naarm. She is studying a Bachelor of Creative
-              Writing at the University of Melbourne and has published poetry with Red Room Poetry and Voiceworks. Her work
-              explores themes of identity, cultural connection and the intricate relationship between land and body. You can
-              find her on Instagram <a href="https://www.instagram.com/bellieeichler" target="_blank" rel="noopener noreferrer">@bellieeichler</a>.
+                Isabella Eichler-Onus is a proud Gunditjmara writer living in Naarm (Melbourne). 
+                She is studying a BA majoring in Creative Writing at the University of Melbourne 
+                and has recently published poetry with various Australian literary magazines. 
+                Her poetry and short fiction explore themes of identity, cultural connection and 
+                the intricate relationship between land and body. She draws inspiration from great 
+                Indigenous writers such as{' '}
+                <a href="https://amzn.to/4jKORjA" target="_blank" rel="noopener noreferrer">Oodgeroo Noonuccal</a>,{' '}
               </p>
               <p>
-              Aenean ut venenatis lorem, sed ultricies risus. Phasellus mattis urna ac arcu rhoncus, in ultricies urna dapibus.
-              Duis vulputate porta augue sed dictum. Donec vitae mauris at dolor suscipit luctus vel sed massa. Aenean ut tempus
-              dolor. Nulla scelerisque maximus efficitur. Vestibulum iaculis quis orci at pharetra. Aenean ultricies felis elementum
-              mauris congue ullamcorper. Maecenas egestas mauris in iaculis varius.
+                <a href="https://amzn.to/40PVUPx" target="_blank" rel="noopener noreferrer">Tony Birch</a>,{' '}
+                <a href="https://amzn.to/3WQmDKt" target="_blank" rel="noopener noreferrer">Evelyn Araluen</a>, and{' '}
+                <a href="https://amzn.to/413z0Wg" target="_blank" rel="noopener noreferrer">Maxine Beneba Clarke</a>,
+                as well as renowned novelists like{' '}
+                <a href="https://amzn.to/4jOukKW" target="_blank" rel="noopener noreferrer">Margaret Atwood</a>,{' '}
+                <a href="https://amzn.to/42KTxzT" target="_blank" rel="noopener noreferrer">Kazuo Ishiguro</a>, and{' '}
+                <a href="https://amzn.to/42I1wOa" target="_blank" rel="noopener noreferrer">Gabrielle Zevin</a>.
+                Alongside her published work, she enjoys experimenting with playful and 
+                unconventional storytelling techniques in personal projects, including 
+                interactive fiction and other multimedia forms. She has many projects planned 
+                for 2025; scroll down to sign up for the mailing list or you can find her 
+                on Instagram{' '}
+                <a href="https://www.instagram.com/bellieeichler" target="_blank" rel="noopener noreferrer">@bellieeichler</a>. 
               </p>
           </section>
         </section>
